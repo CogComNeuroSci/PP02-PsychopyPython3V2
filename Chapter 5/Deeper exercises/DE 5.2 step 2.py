26e0dfb8689dee0e10277debd3b0132db98ba3ee
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2023-11-07 13:10:42 +0100

    update

--- Chapter 5/Deeper exercises/DE 5.2 step 2.py
+++ Chapter 5/Deeper exercises/DE 5.2 step 2.py
@@ -23,9 +23,13 @@ FontColor   = numpy.array([ "red", "blue", "green", "yellow",
                             "red", "blue", "green", "yellow"])
 
 # deduce the congruence
-CongruenceLevels    = numpy.array(["Incongruent", "Congruent"])
-CongruenceBoolean   = numpy.array(ColorWord == FontColor)
-Congruence          = CongruenceLevels[[CongruenceBoolean*1]]
+Congruence = numpy.repeat('Incongruent', len(ColorWord))
+Congruence[ColorWord == FontColor] = 'Congruent'
+
+#Alternative:
+#CongruenceLevels    = numpy.array(["Incongruent", "Congruent"])
+#CongruenceBoolean   = numpy.array(ColorWord == FontColor)
+#Congruence          = CongruenceLevels[CongruenceBoolean*1]
 
 # deduce the task instruction
 if participant%2 == 0:
