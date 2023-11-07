d5dd006ddabaa3f787112a0bbca1e6e1f1a7ff7d
VincentHoofs <85879749+VincentHoofs@users.noreply.github.com>
2023-11-07 13:04:01 +0100

    update

--- Chapter 4/Deeper exercises/DE 4.3.py
+++ Chapter 4/Deeper exercises/DE 4.3.py
@@ -21,9 +21,13 @@ FontColor   = numpy.array([ "red", "blue", "green", "yellow",
                             "red", "blue", "green", "yellow"])
 
 # deduce the congruence
-CongruenceLevels    = numpy.array(["Incongruent", "Congruent"])
-CongruenceBoolean   = numpy.array(ColorWord == FontColor)
-Congruence          = CongruenceLevels[[CongruenceBoolean*1]]
+Congruence = numpy.repeat('Incongruent', len(ColorWord))
+Congruence[ColorWord == FontColor] = 'Congruent'
+
+#Alternative:
+#CongruenceLevels    = numpy.array(["Incongruent", "Congruent"])
+#CongruenceBoolean   = numpy.array(ColorWord == FontColor)
+#Congruence          = CongruenceLevels[CongruenceBoolean*1]
 
 # deduce the correct response
 CorResp = numpy.copy(FontColor)
